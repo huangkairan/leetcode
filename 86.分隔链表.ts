8d5bc2b88d349c0a3dcf1f3faf79f10d6f2ea014
huangkairan <18757128360@163.com>
2022-06-29 17:12:39 +0800

    86

---
+++ 86.分隔链表.ts
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=86 lang=typescript
+ *
+ * [86] 分隔链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function partition(head: ListNode | null, x: number): ListNode | null {
+  let dummy1 = new ListNode(0), dummy2 = new ListNode(0)
+  let p1 = dummy1, p2 = dummy2, p = head
+  while (p !== null) {
+    if (p.val < x) {
+      p1.next = p
+      p1 = p1.next
+    } else {
+      p2.next = p
+      p2 = p2.next
+    }
+    const temp = p.next
+    p.next = null
+    p = temp
+  }
+  p1.next = dummy2.next;
+  return dummy1.next
+};
+// @lc code=end
+
